a7e4de2bba192e9eb269ca7c9216f4afc7e15289
Ryan <Ryan.jhang>
2022-02-21 16:58:49 +0800

    Add flask docker

---
+++ flask/flask_docker/app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+from flask import jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def heelo():
+	resp = jsonify('Hello, World!')
+	resp.status_code = 200
+	return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
